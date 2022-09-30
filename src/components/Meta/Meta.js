8259db493642380f1037c9f359ad9e54700711eb
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-09-30 15:07:18 -0700

    Configure favicon properly

--- src/components/Meta/Meta.js
+++ src/components/Meta/Meta.js
@@ -2,13 +2,38 @@ import Head from 'next/head';
 
 export const Meta = () => (
   <Head>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/favicon/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/favicon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/favicon/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
+    <link
+      rel="mask-icon"
+      href="/favicon/safari-pinned-tab.svg"
+      color="#06b6d4"
+    />
+    <meta name="msapplication-TileColor" content="#603cba" />
+    <link rel="shortcut icon" href="/favicon/favicon.ico" />
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+    <meta name="theme-color" content="#ffffff" />
     <title>GIS Projects | Portfolio of Sara Diercks</title>
     <meta
       name="description"
       content="Portfolio of Sara Diercks, showcasing projects from UCLA Extension's GIS and Geospatial Technology certificate program."
     />
     <link rel="canonical" href="https://saradiercks.com" />
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
   </Head>
 );
