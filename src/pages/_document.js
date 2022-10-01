571120804b2ca0184e2ec6ea869926516c52ce46
Melanie Seltzer <melanieseltzer1@gmail.com>
2022-09-30 21:52:34 -0700

    Fix font loading (#1)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,14 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&display=swap"
           rel="stylesheet"
         />
       </Head>
