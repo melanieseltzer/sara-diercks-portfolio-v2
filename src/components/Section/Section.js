75580efda1da051d22802abcc0565727b90d7753
Melanie Seltzer <melanieseltzer1@gmail.com>
2023-04-13 18:50:31 -0700

    Accessibility and UX updates (#4)
    * Update project card layout
    
    * Use article tag
    
    * Get region role for sections

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,8 +1,11 @@
 import { MaxWidthContainer } from '../MaxWidthContainer';
 
 export const Section = ({ title, children }) => (
-  <MaxWidthContainer as="section" id={title.toLowerCase()}>
-    <h2 className="text-gray-900 mb-6 text-2xl sm:text-3xl font-extrabold tracking-tight">
+  <MaxWidthContainer as="section" aria-labelledby={title.toLowerCase()}>
+    <h2
+      className="text-gray-900 mb-6 text-2xl sm:text-3xl font-extrabold tracking-tight"
+      id={title.toLowerCase()}
+    >
       {title}
     </h2>
 
